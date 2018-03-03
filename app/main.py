777dfbfefddc96414bc69586dfaad38b7eb2c779
Josh <j.larminay@gmail.com>
2018-03-03 09:58:33 -0800

    Added init

--- app/main.py
+++ app/main.py
@@ -158,7 +158,10 @@ def isDangerSquare(data, next, check):
 			return False;
 		else:
 			return False;
-		
+
+@bottle.route('/')
+def static():
+    return "the server is running"			
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -309,4 +312,8 @@ def end():
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app();
 if __name__ == '__main__':
-	bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'));
+	bottle.run(
+		application, 
+		host=os.getenv('IP', '0.0.0.0'), 
+		port=os.getenv('PORT', '8080'),
+		debug = True);
